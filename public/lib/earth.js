2aff292ba5efd89010aa0317fac73c27514045f3
hjk <hjkaddcit@gmail.com>
2019-11-01 19:48:37 +0100

    =test

--- public/lib/earth.js
+++ public/lib/earth.js
@@ -139,7 +139,7 @@
     var transpGeometry = new THREE.SphereGeometry(10, 64, 64);
     var transSphere = new THREE.Mesh(transpGeometry, transpMaterial);
 
-    planet.add(canvasSphere); //this cause slow
+    //planet.add(canvasSphere); //this cause slow
     //planet.add(selCanvasSphere);
     //planet.add(transSphere); 
 
