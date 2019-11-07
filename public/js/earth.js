f030c792df8f6f385df6c318a99b24a52a9ce02d
hjkaddict <hjkaddcit@gmail.com>
2019-11-07 02:44:30 +0100

    =a

--- public/js/earth.js
+++ public/js/earth.js
@@ -8,7 +8,7 @@
     }
 
 
-
+``
 
     //New scene and camera
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
@@ -143,7 +143,7 @@
     var transpGeometry = new THREE.SphereGeometry(10, 64, 64);
     var transpSphere = new THREE.Mesh(transpGeometry, transpMaterial);
 
-    //planet.add(canvasSphere); //this cause slow
+    planet.add(canvasSphere); //this cause slow
     //planet.add(selCanvasSphere);
     planet.add(transpSphere);
 
